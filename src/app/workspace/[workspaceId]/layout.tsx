10dfedf6910aed8e9772380620f60cf6bf4ef890
Dexter Whelan <whelandexter@gmail.com>
2025-06-04 17:34:30 +0100

    wip on the project, next up doing some work on the work space bar. really shows why having figma is handy for this type of thing designing and getting clarity on these things

--- src/app/workspace/[workspaceId]/layout.tsx
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -1,7 +1,15 @@
 "use client"; // components passed as children will not be use client by default meaning we can use server components in this route
 
+import { useState } from "react";
 import { Sidebar } from "./sidebar";
 import { Toolbar } from "./toolbar";
+
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import { WorkspaceSidebar } from "./workspace-sidebar";
 
 interface WorkspaceIdLayoutProps {
   children: React.ReactNode;
@@ -13,7 +21,20 @@ const WorkspaceIdLayout = ({ children }: WorkspaceIdLayoutProps) => {
       <Toolbar />
       <div className="flex h-[calc(100vh-40px)]">
         <Sidebar />
-        {children}</div>
+        <ResizablePanelGroup direction="horizontal" autoSaveId="dxw-workspace-layout">
+          <ResizablePanel
+          defaultSize={20}
+          minSize={11}
+          className="bg-[#5e2c5f]"
+          >
+            <WorkspaceSidebar />
+          </ResizablePanel>
+          <ResizableHandle withHandle/>
+          <ResizablePanel minSize={20}>
+            {children}
+          </ResizablePanel>
+        </ResizablePanelGroup>
+      </div>
     </div>
   );
 };
---
+++ src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -0,0 +1,5 @@
+export const WorkspaceSidebar = () => {
+    return (
+        <div>sidebar</div>
+    )
+}
