ddeb499508cf4335b129ab5feae8aa2028959c73
Dexter Whelan <whelandexter@gmail.com>
2025-06-12 12:47:23 +0100

    added more to workspace sidebar, created channel general on start up for new user, added more to the database, use button ,new api endpoints

--- src/app/workspace/[workspaceId]/workspace-sidebar.tsx
+++ src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -1,37 +1,87 @@
 import { useCurrentMember } from "@/features/members/api/use-current-member";
 import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
-import { useWorkspaceId } from "@/hooks/use-workspace-id"
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import { useGetMembers } from "@/features/members/api/use-get-members";
 
-import { AlertTriangle, Loader, PiSquare } from "lucide-react";
+import {
+  AlertTriangle,
+  HashIcon,
+  Loader,
+  MessageSquareText,
+  PiSquare,
+  SendHorizonal,
+} from "lucide-react";
 
 import { WorkspaceHeader } from "./workspace-header";
+import { SidebarItem } from "./sidebar-item";
+import { useGetChannels } from "@/features/channels/api/use-get-channels";
+import { WorkspaceSection } from "./workspace-section";
+import { UserItem } from "./user-item";
 
 export const WorkspaceSidebar = () => {
-    const workspaceId = useWorkspaceId();
+  const workspaceId = useWorkspaceId();
 
-    const { data:member, isLoading: memberLoading } = useCurrentMember({ workspaceId }); /* interesting note on names here, getting a current user is different from a current member.*/
-    const { data: workspace, isLoading: workspaceLoading} = useGetWorkspace({ id: workspaceId});
+  const { data: member, isLoading: memberLoading } = useCurrentMember({
+    workspaceId,
+  }); /* interesting note on names here, getting a current user is different from a current member.*/
+  const { data: workspace, isLoading: workspaceLoading } = useGetWorkspace({
+    id: workspaceId,
+  });
+  const { data: channels, isLoading: channelsLoading } = useGetChannels({
+    workspaceId,
+  });
+  const { data: members, isLoading: membersLoading } = useGetMembers({
+    workspaceId,
+  });
 
-    if (workspaceLoading || memberLoading) {
-        return(
-            <div className="flex flex-col bg-[#5E2C5F] h-full items-center justify-center">
-                <Loader className="sixe-5 animate-spin text-white"/>
-            </div>
-        );
-    }
-
-    if ( !workspace || !member) {
-        return(
-            <div className="flex flex-col gap-y-2 bg-[#5E2C5F] h-full items-center justify-center">
-                <AlertTriangle className="sixe-5 text-white"/>
-                <p className="text-white text-sm"> Workspace not found</p>
-            </div>
-        );
-    }
-
+  if (workspaceLoading || memberLoading) {
     return (
-        <div className="flex flex-col bg-[#5E2C5F] h-full">
-            <WorkspaceHeader workspace={workspace} isAdmin={member.role === "admin"}/> {/**what happened here exactly, changing quesry from collect to unique allowed the ability to get role from the member data item. why wouldnt we get it before? does collect bring back an object or multiple entries or something? */}
-        </div>
-    )
-}
+      <div className="flex flex-col bg-[#5E2C5F] h-full items-center justify-center">
+        <Loader className="sixe-5 animate-spin text-white" />
+      </div>
+    );
+  }
+
+  if (!workspace || !member) {
+    return (
+      <div className="flex flex-col gap-y-2 bg-[#5E2C5F] h-full items-center justify-center">
+        <AlertTriangle className="sixe-5 text-white" />
+        <p className="text-white text-sm"> Workspace not found</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col bg-[#5E2C5F] h-full">
+      <WorkspaceHeader
+        workspace={workspace}
+        isAdmin={member.role === "admin"}
+      />{" "}
+      {/**what happened here exactly, changing quesry from collect to unique allowed the ability to get role from the member data item. why wouldnt we get it before? does collect bring back an object or multiple entries or something? */}
+      <div className="flex flex-col px-2 mt-3">
+        <SidebarItem label="Threads" icon={MessageSquareText} id="threads" />
+        <SidebarItem label="Drafts & Sent" icon={SendHorizonal} id="drafts" />
+      </div>
+      <WorkspaceSection label="Channels" hint="New Channel" onNew={() => {}}>
+        {channels?.map((item) => (
+          <SidebarItem
+            key={item._id}
+            icon={HashIcon}
+            label={item.name}
+            id={item._id}
+          />
+        ))}
+      </WorkspaceSection>
+      <WorkspaceSection label="Direct Messages" hint="New Direct Message" onNew={() => {}}>
+        {members?.map((item) => (
+          <UserItem
+            key={item._id}
+            id={member._id}
+            label={item.user.name}
+            image={item.user.image}
+          />
+        ))}
+      </WorkspaceSection>
+    </div>
+  );
+};
