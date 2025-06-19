4b32df2c77f4024364e22a506576e1301d5ca328
Dexter Whelan <whelandexter@gmail.com>
2025-06-19 14:33:07 +0100

    sending messages function is done, what a cool complex api series to go through

--- src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -5,13 +5,17 @@ import { useChannelId } from "@/hooks/use-channel-id";
 import { Loader, TriangleAlert } from "lucide-react";
 import { Header } from "./header";
 import { ChatInput } from "./chat-input";
+import { useGetMessages } from "@/features/messages/api/use-get-messages";
 
 const ChannelIdPage = () => {
   const channelId = useChannelId();
 
+  const { results } = useGetMessages({ channelId});
   const { data: channel, isLoading: channelLoading } = useGetIndividualChannels(
     { id: channelId }
   );
+
+  console.log({ results });
 
   if (channelLoading) {
     // this is what they mean when they say an early return. cute idea
@@ -35,7 +39,9 @@ const ChannelIdPage = () => {
 
     <div className="flex flex-col h-full">
       <Header title={channel.name}/>
-      <div className="flex-1"/>
+      <div className="flex-1">
+        {JSON.stringify(results)}
+        </div>
       <ChatInput placeholder={`Message # ${channel.name}`}/>
     </div>
   )
