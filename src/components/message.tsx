6715e6c3ce3cc57cffe7b4c1ac8dd76edc1f6401
Dexter Whelan <whelandexter@gmail.com>
2025-06-25 16:55:35 +0100

    with new users i essentially hve chat function working, added some author functions. need to add load more controls for what each person can do and actually connect some state pieces together to do the work

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -5,6 +5,7 @@ import { format, isToday, isYesterday } from "date-fns";
 import { Hint } from "./hint";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Thumbnail } from "./thumbnail";
+import { Toolbar } from "./toolbar";
 
 const Renderer = dynamic(() => import("@/components/renderer"), { ssr: false });
 
@@ -58,7 +59,7 @@ export const Message = ({
 }: MessageProps) => {
   if (isCompact) {
     return (
-      <div className="flex flex-col gap-2 p-1.5 px-3 hover:bg-color-gray-100/60 group relative">
+      <div className="flex flex-col gap-2 p-1.5 px-3 hover:bg-gray-100/60 group relative">
         <div className="flex items-start gap-2">
           <Hint label={formatFullTime(new Date(createdAt))}>
             <button className="text-xs text-muted-foreground opacity-0 group-hover:opacity-100 w-[40px] leading-[22px] text-center hover:underline">
@@ -80,7 +81,7 @@ export const Message = ({
   const avatarFallback = authorName.charAt(0).toUpperCase();
 
   return (
-    <div className="flex flex-col gap-2 p-1.5 px-5 hover:bg-color-gray-100/60 group relative">
+    <div className="flex flex-col gap-2 p-1.5 px-5 hover:bg-gray-100/60 group relative">
       <div className="flex items-start gap-2">
         <button>
           <Avatar>
@@ -112,6 +113,17 @@ export const Message = ({
           ) : null}
         </div>
       </div>
+      {!isEditing && (
+        <Toolbar 
+        isAuthor={isAuthor}
+        isPending={false}
+        handleEdit={() => setEditingId(id)}
+        handleThread={()=>{}}
+        handleDelete={()=>{}}
+        handleReaction={()=>{}}
+        hideThreadButton={hideThreadButton}
+        />
+      )}
     </div>
   );
 };
---
+++ src/components/toolbar.tsx
@@ -0,0 +1,60 @@
+import { MessageSquareIcon, Pencil, SmileIcon, Trash } from "lucide-react";
+import { Button } from "./ui/button";
+import { Hint } from "./hint";
+import { EmojiPopover } from "./emoji-popover";
+
+interface ToolbarProps {
+  isAuthor: boolean;
+  isPending: boolean;
+  handleEdit: () => void;
+  handleThread: () => void;
+  handleDelete: () => void;
+  handleReaction: (value: string) => void;
+  hideThreadButton?: boolean;
+}
+
+export const Toolbar = ({
+  isAuthor,
+  isPending,
+  handleDelete,
+  handleEdit,
+  handleReaction,
+  handleThread,
+  hideThreadButton,
+}: ToolbarProps) => {
+  return (
+    <div className="absolute top-0 right-5">
+      <div className="group-hover:opacity-100 opacity-0 transition-opacity border bg-white rounded-md shadow-sm">
+        <EmojiPopover
+          hint="Add reaction"
+          onEmojiSelect={(emoji) => handleReaction(emoji.native)}
+        >
+          <Button variant="ghost" size="iconSm" disabled={isPending}>
+            <SmileIcon className="size-4" />
+          </Button>
+        </EmojiPopover>
+        {!hideThreadButton && (
+          <Hint label="Reply in thread">
+            <Button variant="ghost" size="iconSm" disabled={isPending}>
+              <MessageSquareIcon className="size-4" />
+            </Button>
+          </Hint>
+        )}
+        {isAuthor && (
+          <Hint label="Edit message">
+            <Button variant="ghost" size="iconSm" disabled={isPending}>
+              <Pencil className="size-4" />
+            </Button>
+          </Hint>
+        )}
+        {isAuthor && (
+          <Hint label="Delete">
+            <Button variant="ghost" size="iconSm" disabled={isPending}>
+              <Trash className="size-4" />
+            </Button>
+          </Hint>
+        )}
+      </div>
+    </div>
+  );
+};
