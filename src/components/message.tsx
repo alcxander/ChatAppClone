e077a29ddbebccaee0575905549403c200f0ac59
Dexter Whelan <whelandexter@gmail.com>
2025-06-26 20:13:10 +0100

    made infinite load effect on channel messages, that looks sweet, very simple. added multiple reaction addition on message with coloring for different users to see what reactions are theirs or not and also added multiple reaction sections for how to add reactions on a message and new emojipopover section. added reactions api as well

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -11,6 +11,8 @@ import { toast } from "sonner";
 import { cn } from "@/lib/utils";
 import { useRemoveMessage } from "@/features/messages/api/use-remove-message";
 import { useConfirm } from "@/hooks/use-confirm";
+import { useToggleReaction } from "@/features/reactions/api/use-toggle-reaction";
+import { Reactions } from "./reactions";
 
 const Renderer = dynamic(() => import("@/components/renderer"), { ssr: false });
 const Editor = dynamic(() => import("@/components/editor"), { ssr: false });
@@ -71,8 +73,17 @@ export const Message = ({
     useUpdateMessage();
   const { mutate: removeMessage, isPending: isRemovingMessage } =
     useRemoveMessage();
+  const { mutate: toggleReaction, isPending: isTogglingReaction } = useToggleReaction();
 
   const isPending = isUpdatingMessage;
+
+  const handleReaction = (value: string) => {
+    toggleReaction({ messageId: id, value},{
+      onError: () => {
+        toast.error("Failed to toggle reaction");
+      }
+    });
+  };
 
   const handleDelete = async () => {
     const ok = await confirm();
@@ -145,6 +156,9 @@ export const Message = ({
                     (edited)
                   </span>
                 ) : null}
+                <Reactions 
+                data={reactions} onChange={handleReaction}
+              />
               </div>
             )}
           </div>
@@ -155,7 +169,7 @@ export const Message = ({
               handleEdit={() => setEditingId(id)}
               handleThread={() => {}}
               handleDelete={handleDelete}
-              handleReaction={() => {}}
+              handleReaction={handleReaction}
               hideThreadButton={hideThreadButton}
             />
           )}
@@ -215,6 +229,9 @@ export const Message = ({
               {updatedAt ? (
                 <span className="text-xs text-muted-foreground ">(edited)</span>
               ) : null}
+              <Reactions 
+                data={reactions} onChange={handleReaction}
+              />
             </div>
           )}
         </div>
@@ -225,7 +242,7 @@ export const Message = ({
             handleEdit={() => setEditingId(id)}
             handleThread={() => {}}
             handleDelete={handleDelete}
-            handleReaction={() => {}}
+            handleReaction={handleReaction}
             hideThreadButton={hideThreadButton}
           />
         )}
